c504cc75dab85f1763daa90bb7366b9c359c6511
Jay989810 <japhethadamu561@gmail.com>
2025-06-22 09:27:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'JAYDEV', // Your name
-  ownerNumber: '2347089799407', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'NOBLEKIEL', // Your name
+  ownerNumber: '234903946571', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
